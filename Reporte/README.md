cce445020fc0314df722947aace66d1132b6f993
ManuelValdez <manuel@agilgob.com>
2025-01-17 17:43:17 -0600

    Generacion de reporte HTML publicado en jenkins

---
+++ Reporte/README.md
@@ -0,0 +1,35 @@
+
+## Generar Reportes
+
+Sigue los siguientes pasos para generar los reportes:
+
+1. Eliminar reportes anteriores:
+    ```sh
+    rm -r reports
+    ```
+
+2. Ejecutar todos los tests:
+    ```sh
+    npx cypress run --spec .\cypress\e2e\grupo1\actions.cy.js,.\cypress\e2e\grupo1\aliasing.cy.js
+    ```
+
+3. Fusionar los reportes:
+    ```sh
+
+    # Para entornos sin problemas con la codificacion
+    npx mochawesome-merge reports/.jsons/*.json > reports/merged-report.json
+
+    # Para entornos con generacion de archivos diferentes a UTF-8
+    npx mochawesome-merge reports/.jsons/*.json | node -e "const fs = require('fs'); let data = ''; process.stdin.on('data', chunk => data += chunk); process.stdin.on('end', () => fs.writeFileSync('reports/merged-report.json', data, 'utf8'));"
+
+    ```
+
+4. Generar el reporte HTML:
+    ```sh
+    npx marge reports/merged-report.json -reportFilename Reporte.html -reportDir reports --inline true
+    ```
+
+5. Eliminar reportes json:
+    ```sh
+    rm -r reports/.json
+    ```
--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require('cypress');
 const fs = require('fs');
+const { report } = require('process');
 
 
 //Simula la obtención de un token de autenticación
@@ -7,6 +8,14 @@ token = Math.random().toString(30).substring(2, 20).toUpperCase();
 
 module.exports = defineConfig({
   reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    reportDir: 'reports',
+    reportFilename: `[status]-${new Date().toLocaleString('default', { month: 'short' })}${new Date().getDate()}-${new Date().getHours()}${new Date().getMinutes()}`,
+    html: false,
+    json: true,
+    quiet: true,
+  },
+
   env: {
     TOKEN: token,
   },
@@ -23,3 +32,21 @@ module.exports = defineConfig({
     },
   },
 });
+
+
+//rm -r reports
+
+// Ejecutar todos los tests
+// npx cypress run --spec .\cypress\e2e\grupo1\actions.cy.js,.\cypress\e2e\grupo1\aliasing.cy.js  
+
+// Fusionar los reportes
+  // npx mochawesome-merge reports/.jsons/*.json > reports/merged-report.json
+
+  // Windows PowerShell (Evita UTF-16 Little Endian BOM) 
+  // npx mochawesome-merge reports/.jsons/*.json | node -e "const fs = require('fs'); let data = ''; process.stdin.on('data', chunk => data += chunk); process.stdin.on('end', () => fs.writeFileSync('reports/merged-report.json', data, 'utf8'));"
+
+// Generar el reporte HTML
+// npx marge reports/merged-report.json -reportFilename Reporte.html -reportDir reports --inline true 
+
+// Eliminar reportes json
+// rm -r reports/.json
